1699f9cba51557e6a84068f994362f37b80c6664
danielbrechner <brechner.daniel@gmail.com>
2022-12-24 12:32:50 -0500

    added ipynb

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from data import data
 with open("hangboard_data.csv", mode='w', newline="") as csvfile:
     feildnames = data[0].keys()
     writer = csv.DictWriter(csvfile, fieldnames=feildnames)
+    writer.writeheader()
     writer.writerows(data)
