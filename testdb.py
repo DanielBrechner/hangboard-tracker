96cd0b5118b7aa0dd0da8969d4215c287c72ea2d
Daniel Brechner <brechner.daniel@gmail.com>
2023-02-03 07:59:29 -0500

    added values to db

--- testdb.py
+++ testdb.py
@@ -1,3 +1,16 @@
 import sqlite3
 
 conn = sqlite3.connect("hang_record.db")
+
+# create a cursor
+c = conn.cursor()
+
+
+c.execute(
+    "INSERT INTO workout VALUES ('Daniel','Brechner','brechner.daniel@gmail.com')")
+
+# commit our command
+conn.commit()
+
+# close connection
+conn.close()
